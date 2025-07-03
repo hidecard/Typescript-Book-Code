be5b2cf7a6a3e0f516044a85b9f49f8c97978071
hidecard <hidecard1500@gmail.com>
2025-07-03 10:29:16 +0630

    Module 1

---
+++ Module2/lesson.ts
@@ -0,0 +1,10 @@
+let age: number = 25;
+let nameS: string = "Aung Aung";
+let isStudent: boolean = true;
+let unknownValue: any = "Could be anything";
+let nothing: void = undefined;
+ 
+console.log(age); // 25
+console.log(nameS); // Aung Aung
+console.log(isStudent); // true
+console.log(unknownValue); // Could be anything
