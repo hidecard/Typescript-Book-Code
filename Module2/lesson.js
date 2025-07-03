399e68a727009bbe103fd4daf9c5a157fb396cb5
hidecard <hidecard1500@gmail.com>
2025-07-03 10:33:32 +0630

    Module2

---
+++ Module2/lesson.js
@@ -0,0 +1,9 @@
+var age = 25;
+var nameS = "Aung Aung";
+var isStudent = true;
+var unknownValue = "Could be anything";
+var nothing = undefined;
+console.log(age); // 25
+console.log(nameS); // Aung Aung
+console.log(isStudent); // true
+console.log(unknownValue); // Could be anything
