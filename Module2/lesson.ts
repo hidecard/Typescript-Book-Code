d7a04d2df0f69ef5d22d5b3afafbfbda9f4265a3
hidecard <hidecard1500@gmail.com>
2025-07-03 11:05:59 +0630

    function

--- Module2/lesson.ts
+++ Module2/lesson.ts
@@ -15,11 +15,22 @@
 // numbers.push(5); // အဆင်ပြေပါတယ်
 // numbers.push("6"); // အမှား: Argument of type 'string' is not assignable to parameter of type 'number'
 
-let person: [string, number] = ["Mg Mg", 25];
-console.log(person); // ["Mg Mg", 25]
-person[0] = "Aung Aung"; // အဆင်ပြေပါတယ်
-person[1] = "30"; // အမှား: Type 'string' is not assignable to type 'number'
+// let person: [string, number] = ["Mg Mg", 25];
+// console.log(person); // ["Mg Mg", 25]
+// person[0] = "Aung Aung"; // အဆင်ပြေပါတယ်
+// person[1] = "30"; // အမှား: Type 'string' is not assignable to type 'number'
 
 
-let count = 10; // TypeScript က count ကို number အဖြစ် ခန့်မှန်းပါတယ်
-count = "ten"; // အမှား: Type 'string' is not assignable to type 'numbe
+// let count = 10; // TypeScript က count ကို number အဖြစ် ခန့်မှန်းပါတယ်
+// count = "ten"; // အမှား: Type 'string' is not assignable to type 'number
+
+
+
+// function အပိုင်း
+
+// function greet(name: string, age: number): string {
+//   return `Hello, ${name}! You are ${age} years old.`;
+// }
+// console.log(greet("Mg Mg", 25)); // Hello, Mg Mg! You are 25 years old.
+// console.log(greet("Mg Mg", "30")); // အမှား: Argument of type 'string' is not assignable to parameter of type 'number' 
+
